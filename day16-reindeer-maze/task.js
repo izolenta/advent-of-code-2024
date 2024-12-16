62f0f4217a4a7fc67c1217f7e47b49f9802a3456
Alexey Sharov <lehtenstaarn@gmail.com>
2024-12-16 10:21:34 +0100

    day16

--- day16-reindeer-maze/task.js
+++ day16-reindeer-maze/task.js
@@ -97,10 +97,10 @@ const star2 = () => {
 
 const main = () => {
     console.time('star1');
-    console.log(`⚝: ${star1()}`);
+    console.log(`🎄: ${star1()}`);
     console.timeEnd('star1');
     console.time('star2');
-    console.log(`⚝⚝: ${star2()}`);
+    console.log(`🎄🎄: ${star2()}`);
     console.timeEnd('star2');
 };
 
